e1553c1883c5d929d4f6f3e4b25f6c278f96c8e9
southwane <southwane223@gmail.com>
2024-03-23 17:24:27 -0400

    file changes

--- javascript/script1.js
+++ javascript/script1.js
@@ -1,18 +1,18 @@
 
-const menuToggle = document.getElementById("menu-toggle")
+// const menuToggle = document.getElementById("menu-toggle")
 
-menuToggle.addEventListener('click', toogleMenu)
+// menuToggle.addEventListener('click', toogleMenu)
 
 
-function toogleMenu () {
-const menu = document.getElementById("menu");
-if (menu.style.display === "flex") {
-  menu.style.display ==="none"
-}else {
-  menu.style.display="flex"
-}
-console.log('open')
-}
+// function toogleMenu () {
+// const menu = document.getElementById("menu");
+// if (menu.style.display === "flex") {
+//   menu.style.display ==="none"
+// }else {
+//   menu.style.display="flex"
+// }
+// console.log('open')
+// }
 
 
 
