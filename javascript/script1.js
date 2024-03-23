c77f3d94a65feb964eac24088bcff9f3ddfc6385
southwane <southwane223@gmail.com>
2024-03-23 17:18:29 -0400

    file changes

--- javascript/script1.js
+++ javascript/script1.js
@@ -6,12 +6,12 @@ menuToggle.addEventListener('click', toogleMenu)
 
 function toogleMenu () {
 const menu = document.getElementById("menu");
-console.log('open')
-  if (menu.style.display === "flex") {
-menu.style.display="none"
+if (menu.style.display === "flex") {
+  menu.style.display ==="none"
 }else {
-menu.style.display="flex"
+  menu.style.display="flex"
 }
+console.log('open')
 }
 
 
